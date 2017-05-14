885f228ed1881b5f5c0945bcec8c322d5262395e
Michael <mikeabrownjr@gmail.com>
2017-05-13 23:59:41 -0400

    Added a new activity for when the submit button is clicked. Planning on creating  a concluding splash screen that displays the final score and a FAB to reset the app to start over.

--- app/src/main/java/com/example/android/genesisquiz/MainActivity.java
+++ app/src/main/java/com/example/android/genesisquiz/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.android.genesisquiz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+
+    int quizPoints = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitQuiz(View view){
+
+        CheckBox correctRivers = (CheckBox) findViewById(R.id.correctRivers);
+        if (correctRivers.isChecked()){
+            quizPoints +=1;
+        }
+
+        CheckBox wrongRivers = (CheckBox) findViewById(R.id.wrongRivers);
+        if (wrongRivers.isChecked()){
+            quizPoints -=1;
+        }
+
+        CheckBox wrongRivers2 = (CheckBox) findViewById(R.id.wrongRivers2);
+        if (wrongRivers2.isChecked()){
+            quizPoints -=1;
+        }
+
+        CheckBox trueTrees = (CheckBox) findViewById(R.id.trueTrees);
+        if (trueTrees.isChecked()){
+            quizPoints+=1;
+        }
+
+        CheckBox falseTrees = (CheckBox) findViewById(R.id.falseTrees);
+        if(falseTrees.isChecked()){
+            quizPoints-= 1;
+        }
+
+        CheckBox trueRest = (CheckBox) findViewById(R.id.trueRest);
+        if(trueRest.isChecked()){
+            quizPoints -= 1;
+        }
+
+        CheckBox falseRest = (CheckBox) findViewById(R.id.falseRest);
+        if(falseRest.isChecked()){
+            quizPoints+=1;
+        }
+
+
+        Intent intent = new Intent(this, FinalScoreActivity.class);
+        startActivity(intent);
+
     }
 }
