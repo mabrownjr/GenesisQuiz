9b1eb48727857c76dc2b69d5aca4fb3b6a8bd5ff
Michael <mikeabrownjr@gmail.com>
2017-05-15 00:36:34 -0400

    Had to start from scratch. using radio groups and buttons instead now. ALso added a reset button to reset the score once pressed.

--- app/src/main/java/com/example/android/genesisquiz/MainActivity.java
+++ app/src/main/java/com/example/android/genesisquiz/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.android.genesisquiz;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    RadioGroup rd_Rivers, rd_Trees, rd_Rest;
+    RadioButton correctRivers, wrongRivers, wrongRivers2, trueTree, falseTree;
+    EditText forbiddenTree, creationPart;
+
     int quizPoints = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,45 +25,62 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitQuiz(View view){
+        quizPoints += question1();
+        quizPoints += question2();
+        quizPoints += question3();
 
-        CheckBox correctRivers = (CheckBox) findViewById(R.id.correctRivers);
-        if (correctRivers.isChecked()){
-            quizPoints +=1;
+        Context context = getApplicationContext();
+        CharSequence text = "You have " + quizPoints + " points correct.";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+    }
+    public void resetQuiz(View view){
+        rd_Rivers = (RadioGroup) findViewById(R.id.radioRivers);
+        rd_Trees = (RadioGroup) findViewById(R.id.radioTrees);
+        rd_Rest= (RadioGroup) findViewById(R.id.radioRest);
+
+        rd_Rivers.clearCheck();
+        rd_Trees.clearCheck();
+        rd_Rest.clearCheck();
+
+        quizPoints = 0;
+    }
+    public int question1(){
+        correctRivers = (RadioButton) findViewById(R.id.correctRivers);
+        wrongRivers = (RadioButton) findViewById(R.id.wrongRivers);
+        wrongRivers2 = (RadioButton) findViewById(R.id.wrongRivers2);
+
+
+        if(correctRivers.isChecked()){
+            return 1;
         }
 
-        CheckBox wrongRivers = (CheckBox) findViewById(R.id.wrongRivers);
-        if (wrongRivers.isChecked()){
-            quizPoints -=1;
+        return 0;
+    }
+    public int question2(){
+        trueTree = (RadioButton) findViewById(R.id.trueTrees);
+
+        if (trueTree.isChecked()){
+            return 1;
         }
 
-        CheckBox wrongRivers2 = (CheckBox) findViewById(R.id.wrongRivers2);
-        if (wrongRivers2.isChecked()){
-            quizPoints -=1;
+        return 0;
+    }
+    public int question3(){
+
+        String answer = "Tree of Knowledge of Good and Evil";
+        forbiddenTree = (EditText) findViewById(R.id.forbiddenTree);
+        String input = forbiddenTree.toString();
+
+        input = input.replaceAll("\\s", "");
+
+        if(input == answer.replaceAll("\\s", "")){
+            return 1;
         }
 
-        CheckBox trueTrees = (CheckBox) findViewById(R.id.trueTrees);
-        if (trueTrees.isChecked()){
-            quizPoints+=1;
-        }
-
-        CheckBox falseTrees = (CheckBox) findViewById(R.id.falseTrees);
-        if(falseTrees.isChecked()){
-            quizPoints-= 1;
-        }
-
-        CheckBox trueRest = (CheckBox) findViewById(R.id.trueRest);
-        if(trueRest.isChecked()){
-            quizPoints -= 1;
-        }
-
-        CheckBox falseRest = (CheckBox) findViewById(R.id.falseRest);
-        if(falseRest.isChecked()){
-            quizPoints+=1;
-        }
-
-
-        Intent intent = new Intent(this, FinalScoreActivity.class);
-        startActivity(intent);
-
+        return 0;
     }
 }
